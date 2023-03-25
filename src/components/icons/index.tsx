755f284cfae88390cf0c1af702cf6eebe7219f5c
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2023-03-24 17:04:24 -0700

    fix: more import order

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,8 +1,8 @@
 import { default as DefineIcon } from "./DefineIcon";
+import { default as DeliverIcon } from "./DeliverIcon";
 import { default as DiscoverIcon } from "./DiscoverIcon";
 import { default as DoIcon } from "./DoIcon";
-import { default as DeliverIcon } from "./DeliverIcon";
 import { default as InstagramIcon } from "./InstagramIcon";
 
-export { DefineIcon, DiscoverIcon, DoIcon, DeliverIcon, InstagramIcon };
+export { DefineIcon, DeliverIcon, DiscoverIcon, DoIcon, InstagramIcon };
 
--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,6 +1,5 @@
 export const getCloudinaryImageUrl = (publicId: string): string => {
   const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   const baseUrl = `https://res.cloudinary.com/${cloudName}/image/upload`;
   const options = {
     quality: "auto",
