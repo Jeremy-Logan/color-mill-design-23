7c7e368232870329e105420c2d5f1c39c249fcec
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2023-04-03 12:46:48 -0700

    fix: contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -34,6 +34,7 @@ const ContactForm: React.FC = () => {
       onSubmit={handleSubmit(onSubmit)}
       method="POST"
       name="contact"
+      action="color-mill-design-23"
       data-netlify="true"
       netlify-honeypot="bot-field"
       id="contact"
@@ -144,7 +145,6 @@ const ContactForm: React.FC = () => {
       </div>
       <button
         type="submit"
-        onClick={handleSubmit(onSubmit)}
         className="duration-250 mt-4 w-full transform bg-[#fae06a] py-4 px-2 text-xl font-semibold text-gray-800 shadow-md transition hover:scale-105 hover:bg-[#94df57] hover:shadow-lg md:w-1/3"
       >
         Submit
